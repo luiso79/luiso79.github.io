583e6edd0744ffd2adffdc733f4868f44075e80e
luiso79 <32992232+luiso79@users.noreply.github.com>
2023-02-28 22:53:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+/*** dependiendo el boton que elijas en la barra lateral se carga el 
+ * correspondiente html en el iframe "dinamico" ***/
+
+document.getElementById("boton_datos").onclick=function(){
+    document.getElementById("dinamico").src="datos.html";
+    }
+    
+document.getElementById("boton_estudios").onclick=function(){
+    document.getElementById("dinamico").src="estudios.html";
+    }    
+
+document.getElementById("boton_experiencia").onclick=function(){
+    document.getElementById("dinamico").src="experiencia.html";
+    }
+
+document.getElementById("boton_habilidades").onclick=function(){
+    document.getElementById("dinamico").src="habilidades.html";
+    }
+    
